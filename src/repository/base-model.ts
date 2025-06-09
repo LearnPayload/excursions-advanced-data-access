05967c8c30c02f0eb3469af5b5e8a0f6a19177f2
Colyn Brown <colyn.brown@gmail.com>
2025-06-09 17:07:31 -0400

    Implement BaseModel class for standardized data access methods

---
+++ src/repository/base-model.ts
@@ -0,0 +1,70 @@
+import { getPayloadClient } from '@/db/client'
+import { notFound } from 'next/navigation'
+import { CollectionSlug, PaginatedDocs, Where } from 'payload'
+
+export abstract class BaseModel<T> {
+  abstract collection: CollectionSlug
+  async getBySlugOrFail(slug: string) {
+    const doc = await this.getBySlug(slug)
+
+    if (!doc) {
+      notFound()
+    }
+
+    return doc
+  }
+  async getBySlug(slug: string): Promise<T | null> {
+    const payload = await getPayloadClient()
+
+    const result = (await payload.find({
+      collection: this.collection,
+      where: { slug: { equals: slug } },
+      limit: 1,
+    })) as PaginatedDocs<T>
+
+    return result.docs[0] ?? null
+  }
+  async getAll(where: Where = {}): Promise<T[]> {
+    const payload = await getPayloadClient()
+
+    const result = (await payload.find({
+      collection: this.collection,
+      where,
+    })) as PaginatedDocs<T>
+
+    return result.docs ?? []
+  }
+  async getByID(id: number | string) {
+    const payload = await getPayloadClient()
+
+    const result = (await payload.findByID({
+      collection: this.collection,
+      id,
+    })) as T
+
+    return result
+  }
+
+  async create(data: Omit<Partial<T>, 'id' | 'createdAt' | 'updatedAt'>) {
+    const payload = await getPayloadClient()
+    await payload.create({
+      collection: this.collection,
+      data,
+    })
+  }
+  async update(id: string | number, data: Omit<Partial<T>, 'id' | 'createdAt' | 'updatedAt'>) {
+    const payload = await getPayloadClient()
+    await payload.update({
+      collection: this.collection,
+      id,
+      data,
+    })
+  }
+  async delete(id: string | number) {
+    const payload = await getPayloadClient()
+    await payload.delete({
+      collection: this.collection,
+      id,
+    })
+  }
+}
